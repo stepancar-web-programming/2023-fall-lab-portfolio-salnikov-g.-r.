06be86764aee3970070bd4d0419dcad42674bb50
goshik033 <sk.gosha@yandex.ru>
2023-11-10 14:52:15 +0300

    Добавлен эффект печатной машинки

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ $(window).on('load', function(){
     });
 
 });
+
+let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
+    stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
+    typeSpeed: 80, // Скорость печати
+    startDelay: 500, // Задержка перед стартом анимации
+    backSpeed: 50, // Скорость удаления
+    showCursor: true,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+    loop: true, // Указываем, повторять ли анимацию
+    loopCount: false,
+    
+});
